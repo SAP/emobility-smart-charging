1054322da9a579695bce8083086726bcd8a4e23b
Oliver Frendo <oliver.frendo@sap.com>
2021-03-05 17:40:44 +0100

    Add optimization settings to the REST API. Peak shaving/energy price optimization can now be used via the REST API

--- src/main/java/com/sap/charging/server/EmobilitySmartChargingApplication.java
+++ src/main/java/com/sap/charging/server/EmobilitySmartChargingApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmobilitySmartChargingApplication {
 
-	public static int verbosity;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(EmobilitySmartChargingApplication.class, args);
 	}
